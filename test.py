21e70aaaabc0314336c42acd33ec73b46043cc3c
nxcon <114954840+nxcon@users.noreply.github.com>
2023-05-09 10:18:47 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,11 @@ import requests
 
 # this is a fake id to test.
 
+
+github token = ghp_RE4gU3zgpvNqN5w3jgnA2tnHQDLxXetbqDLq  
+ AWS_ID = AKIAWARWDKHNHMZTLY4I 
+AWS_SECRET = AKIAIOSFODAN7E2ASPL2 
+
 aws_id= AKIAWARWDKHNHMZTLY4I
 
 print(aws_id)
